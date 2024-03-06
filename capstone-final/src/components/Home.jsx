cb98bb5e63f1d9b6d85cb9b6e5d67b232858c3f4
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-06 13:55:31 -0600

    drafted home page banner and title

---
+++ capstone-final/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import home_cyber from "../assets/cyberhome.jpg";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Binary Bulwark</h1>
+            <img src={home_cyber} />
+         </div>
+    );
+  }
+
+  
+  export default Home;
+  
