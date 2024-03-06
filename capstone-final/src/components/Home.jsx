e06e824848a9be7fe08c7bddda347c91e5aa6ee4
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-06 16:22:10 -0600

    finished home page description and banner for unauthenticated user

--- capstone-final/src/components/Home.jsx
+++ capstone-final/src/components/Home.jsx
@@ -5,6 +5,12 @@ function Home() {
         <div>
             <h1>Welcome to Binary Bulwark</h1>
             <img src={home_cyber} />
+            <p> Defending against hackers is difficult. They only need to exploit 1 vulnerability,
+                while you have to defend against all.  
+            </p>
+            <p>With specialized services tailored to business needs, 
+               BinaryBulwark solves your information security problems.
+            </p>
          </div>
     );
   }
