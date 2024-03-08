75f9c4e5dddd5a7282ff133d4ee20285618267d8
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-07 19:27:58 -0600

    added Register component

--- capstone-final/src/App.jsx
+++ capstone-final/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 //componenets
-import Home from './components/Home'
+import Home from "./components/Home";
+import Register from "./components/Register";
 
 function App() {
   return (
         <div>
           <Home />
+          <Register />
         </div>
   );
 }
