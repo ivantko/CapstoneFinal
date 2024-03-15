b14d56056b8ea93a15b01a4cfc9a83669536b85f
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-14 21:02:32 -0500

    added sloginn.jsx components

--- capstone-final/src/components/Login.jsx
+++ capstone-final/src/components/Login.jsx
@@ -1,33 +1,32 @@
 import { useState } from "react";
 
-function Login (){
+function Login (props) {
     const [userInfo, setUserInfo] = useState({
         username: "", 
         password: "", 
        });
     const [errorMsg, setError] = useState(null);
-
+    
 
     const eventHandler = async (event) => {
         event.preventDefault();
         // const{data, error} = await register(userInfo);
    
-        if(error){
+        // if(error){
         
-            // setError(error.data.message)
-            // console.log(`error ${JSON.stringify(error.data.message)}`);
-        } else {
+        //     // setError(error.data.message)
+        //     // console.log(`error ${JSON.stringify(error.data.message)}`);
+        // } else {
              
-            // props.setid(data.id)
-            // console.log(`data ${JSON.stringify(data.id)}`);
-        }
+        //     // props.setid(data.id)
+        //     // console.log(`data ${JSON.stringify(data.id)}`);
+        // }
     };
 
     const onUserInput = (e) => {
         if(errorMsg){
             setError(null);
         }
-
 
         setUserInfo({...userInfo, [e.target.name]: e.target.value });
     };
@@ -36,7 +35,6 @@ function Login (){
 return (
     <div>
         <h2>Login</h2>
-        {/* error message  */}
         {errorMsg ? <p>{errorMsg}</p> : <span />}
         <form onSubmit={eventHandler}>
             <label>
@@ -47,25 +45,12 @@ return (
                 Password
                 <input type="password" placeholder="Password" name="password" value={userInfo.password} onChange={onUserInput}/>
             </label>
-            <label>
-                Email
-                <input type="text" placeholder="Email" name="email" value={userInfo.email} onChange={onUserInput} />
-            </label>
-            <label>
-                First Name
-                <input type="text" placeholder="First Name" name="first_name" value={userInfo.first_name} onChange={onUserInput} />
-            </label>
-            <label>
-                Last Name
-                <input type="text" placeholder="Last Name" name="last_name" value={userInfo.last_name} onChange={onUserInput} />
-            </label>
+
             <button type="submit">Submit</button>
         </form>
     </div>
     );
 
 }
-
-
 
 export default Login;
