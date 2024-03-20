1a1098fc7a096429ad5616fa651fc88bfa168683
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-20 08:16:12 +0000

    plant list created

--- capstone-final/src/components/Services.jsx
+++ capstone-final/src/components/Services.jsx
@@ -1,3 +1,4 @@
+// import { isPlainObject } from "@reduxjs/toolkit";
 import { useServicesQuery } from "../redux/api";
 
 function Services({ token }) {
@@ -13,9 +14,37 @@ function Services({ token }) {
     }
 
     console.log("DATA", data)
-    console.log(data[3])
 
-    return <h2>Services</h2>; 
+    //Product object example within data array:
+    // {
+    //     "id": 1,
+    //     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    //     "price": 109.95,
+    //     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    //     "category": "men's clothing",
+    //     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    //     "rating": {
+    //       "rate": 3.9,
+    //       "count": 120
+    //     }
+
+
+    return (
+    <div>
+    <h2>Services</h2> 
+    {data.map((service) => {
+        return (
+        <div key={service.id}>
+            <h2>{service.title}</h2>
+            <img src={service.image} />
+            <p>Price: ${service.price}</p>
+            <p>Rating: {service.rating.rate} ({service.rating.count} Reviews)</p>
+            <p>Description: {service.description}</p>
+            </div>
+        );
+    })}
+    </div>
+    )
 }
 
 export default Services;
