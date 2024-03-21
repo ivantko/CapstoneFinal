f48bb1f8906d929d415f64357f6bf7340725350d
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-21 09:35:53 -0500

    fixed image tape

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,12 @@
 import home_cyber from "../assets/Banner1.webp";
 import cloud1 from "../assets/cloud1.webp";
+import consulting from "../assets/consulting.webp";
+import consulting1 from "../assets/consulting1.webp";
+import forensics from "../assets/forensics.webp";
+import pentest from "../assets/KNIGHT.webp";
+import pentest2 from "../assets/pentest3.webp";
+import assessment from "../assets/army.webp";
+
 
 function Home() {
     return (
@@ -7,23 +14,24 @@ function Home() {
             <img src={home_cyber} alt="Cyber Army" />
             <h3>Cybersecurity consulting services
             </h3>
-            <ImageTape /> {/* Add this line to include the ImageTape component */}
+            <ImageTape /> 
          </div>
     );
   }
 
+
+  //using rolling image tape to showcase/hint services offered
   function ImageTape() {
     return (
       <div className="image-tape">
         <div className="image-container">
           <img src={cloud1} alt="Cloud"/>
-          <img src="/src/assets/consulting.webp" alt="consulting" />
-          <img src="/src/assets/consulting1.webp" alt="consulting1" />
-          <img src="/src/assets/forensics.webp" alt="forensics" />
-          <img src="/src/assets/KNIGHT.webp" alt="pentest" />
-          <img src="/src/assets/pentest3.webp" alt="pentest2" />
-          <img src="/src/assets/army.webp" alt="assessment" />
-          {/* Add more images as needed */}
+          <img src={consulting} alt="consulting" />
+          <img src={consulting1} alt="consulting1" />
+          <img src={forensics} alt="forensics" />
+          <img src={pentest} alt="pentest" />
+          <img src={pentest2} alt="pentest2" />
+          <img src={assessment} alt="assessment" />
         </div>
       </div>
     );
