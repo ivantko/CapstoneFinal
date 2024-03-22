1d7e20fee41a5ed85b96010d35f4ac734bdf2553
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-03-22 11:58:32 +0000

    added console logs for handleChange in addservice

--- src/components/AddService.jsx
+++ src/components/AddService.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 
 function AddService() {
-    const [form, setForm] = useState()
-    const [error, setError] = useState({title: "", price: "", description: "", image_url:"" });
+    const [form, setForm] = useState({
+        title: "", price: "", description: "", image_url:""}
+    )
+    const [error, setError] = useState(null);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log("here");
+        console.log("here", form.title);
+        console.log('************')
     };
+
+    const handleChange = (event) => {
+        console.log("target", event.target.target)
+        console.log("value", event.target.value)
+        console.log('************')
+    }
 
     return (
         <section>
             <h2>Add Service</h2>
             <p>All items marked with * are required</p>
             <label htmlFor="title">Name</label>
-            <input name="title" />
+            <input name="title" onChange={handleChange} />
             <label htmlFor="price">Price</label>
             <input name="price" />
             <label htmlFor="description">Description</label>
